28e11871f9c1db59e464a9c34f94f2d6df24ead2
venilson2 <venilson2009@hotmail.com>
2025-04-26 17:40:57 -0300

    Adiciona dependências para validação de formulários e gerenciamento de estado no Login. Inclui "react-hook-form" e "yup" no package.json e package-lock.json. Cria o hook useLogin para gerenciar a lógica de autenticação com validação de dados.

---
+++ src/pages/authentication/Login/useLogin.tsx
@@ -0,0 +1,70 @@
+import HttpClient from "@/api/httpClient"
+import { useSearchParams } from "react-router-dom"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { toast } from "sonner"
+import { useAuthContext } from "@/context/useAuthContext"
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+
+export default function useLogin() {
+	const [loading, setLoading] = useState(false)
+	const navigate = useNavigate()
+	const [searchParams] = useSearchParams()
+
+	const { isAuthenticated, saveSession } = useAuthContext()
+
+	const schemaResolver = yup.object().shape({
+		email: yup
+			.string()
+			.email('Por favor, insira um e-mail válido')
+			.required('Por favor, insira seu e-mail'),
+		password: yup.string().required('Por favor, insira sua senha'),
+	})
+
+	const { control, handleSubmit } = useForm({
+		resolver: yupResolver(schemaResolver),
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	})
+
+	type LoginFormFields = yup.InferType<typeof schemaResolver>
+
+	const redirectUrl = searchParams.get('next') ?? '/dashboards/analytics'
+
+	const login = handleSubmit(async function (values: LoginFormFields) {
+		setLoading(true)
+		try {
+			const res = await HttpClient.post('auth/login', {
+				'email': values.email,
+				'password': values.password,
+			})
+			if (res.data.token) {
+				saveSession({
+					...(res.data ?? {}),
+					token: res.data.token,
+				})
+				toast.success('Login efetuado com sucesso, redirecionando...', {
+					position: 'top-right',
+					duration: 2000,
+				})
+				navigate(redirectUrl)
+			}
+		} catch (e: any) {
+			if (e.response?.data?.error) {
+				toast.error(e.response?.data?.error, {
+					position: 'top-right',
+					duration: 2000,
+				})
+			}
+		} finally {
+			setLoading(false)
+		}
+	})
+
+	return { loading, login, redirectUrl, isAuthenticated, control }
+}
