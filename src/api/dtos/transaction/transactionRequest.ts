8c788e24167c0f7ac7083527bfacfca66853ca32
venilson2 <venilson2009@hotmail.com>
2025-05-04 16:54:45 -0300

    Adiciona novos campos à interface TransactionRequest

--- src/api/dtos/transaction/transactionRequest.ts
+++ src/api/dtos/transaction/transactionRequest.ts
@@ -1,8 +1,13 @@
 export interface TransactionRequest {
+  id?: string;
   depositedDate: string;
   description: string;
   walletId: string;
   categoryId: string;
   amount: number;
   transferType: "DEBIT" | "CREDIT";
+  isInstallment: boolean | null;
+  installmentNumber: number | null;
+  installmentInterval: "MONTHLY" | "YEARLY" | "WEEKLY" | "DAILY" | null;
+  isRecurring: boolean | null;
 }
